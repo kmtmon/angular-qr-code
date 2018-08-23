bba449ff86610913bec5aaa346768f84c58a69a3
Kaung Myat Thet Mon <kaungmyatthetmon@gmail.com>
2018-08-23 21:43:43 +0800

    Customer signature for delivered item

--- src/app/item-details/item-details.component.ts
+++ src/app/item-details/item-details.component.ts
@@ -1,30 +1,33 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Location } from '@angular/common';
-import { MessageService } from '../services/message.service'; 
-import {ItemService} from '../services/item.service';
-import {Item} from '../models/item';
-import {Category } from '../models/category';
-import {CategoryService} from '../services/category.service';
+import { MessageService } from '../services/message.service';
+import { ItemService } from '../services/item.service';
+import { Item } from '../models/item';
+import { Category } from '../models/category';
+import { CategoryService } from '../services/category.service';
 import { AngularFirestore, AngularFirestoreCollection }
   from 'angularfire2/firestore';
-import {CreateItems} from '../services/createItems';
-import {CreateCategory} from '../services/createCategory';
+import { CreateItems } from '../services/createItems';
+import { CreateCategory } from '../services/createCategory';
 @Component({
   selector: 'app-item-details',
   templateUrl: './item-details.component.html',
   styleUrls: ['./item-details.component.css']
 })
 export class ItemDetailsComponent implements OnInit {
-  @Input() 
-  itemId:string;
+  @Input()
+  itemId: string;
   item: Item;
-  cat:string;
-  catId:string;
-  remark:string;
-  status:string;
-  categories=this.createCat.cats;
-  description:string;
+  cat: string;
+  catId: string;
+  remark: string;
+  status: string;
+  categories = this.createCat.cats;
+  description: string;
+  displaySignature = false;
+  signatureURL = '';
+
   constructor(
     private route: ActivatedRoute,
     private itemService: ItemService,
@@ -33,25 +36,35 @@ export class ItemDetailsComponent implements OnInit {
     private editrouter: Router,
     private catService: CategoryService,
     private afs: AngularFirestore,
-    private createItems:CreateItems,
-    private createCat:CreateCategory
+    private createItems: CreateItems,
+    private createCat: CreateCategory
   ) { }
 
   ngOnInit() {
-    this. getItem();
-    this. getItemCat();
-    
+    this.getItem();
+    this.getItemCat();
+
   }
 
 
   getItem(): void {
     this.itemId = this.route.snapshot.paramMap.get('id');
-   // this.item=this.itemService.getItem(id); 
- 
-    if(this.createItems.items.length != 0){
-      this.item=this.itemService.getItem(this.itemId); 
+    // this.item=this.itemService.getItem(id); 
+
+    if (this.createItems.items.length != 0) {
+      this.item = this.itemService.getItem(this.itemId);
+      this.displaySignatureImage()
     }
-    
+
+  }
+
+  displaySignatureImage() {
+    if (this.item.status === 'Delivered') {
+      if (this.item.location !== undefined) {
+        this.displaySignature = true
+        this.signatureURL = this.item.location
+      }
+    }
   }
 
   goBack(): void {
@@ -59,49 +72,49 @@ export class ItemDetailsComponent implements OnInit {
   }
 
   editItem() {
-    this.editrouter.navigateByUrl('/itemEdit/'+this.itemId);
+    this.editrouter.navigateByUrl('/itemEdit/' + this.itemId);
   }
-  
-  getItemCat(){
-    if(this.createItems.items.length == 0){
+
+  getItemCat() {
+    if (this.createItems.items.length == 0) {
       let itemDoc = this.afs.firestore.collection(`item`);
-      itemDoc.get().then((querySnapshot) => { 
+      itemDoc.get().then((querySnapshot) => {
         querySnapshot.forEach((doc) => {
-            this.createItems.addToItemList(doc.id,doc.get('productID'),doc.get('remark'),doc.get('status'),doc.get('description'));
-          })
-      })   
+          this.createItems.addToItemList(doc.id, doc.get('productID'), doc.get('remark'), doc.get('status'), doc.get('description'));
+        })
+      })
     }
-    let str ="";
-    if(this.categories.length==0){
+    let str = "";
+    if (this.categories.length == 0) {
       str = localStorage.getItem('currentItem');
-      let strs=str.split(',');
-      this.cat=strs[0];
-      this.remark=strs[1];
-      this.status=strs[2];
-      this.description=strs[3];
+      let strs = str.split(',');
+      this.cat = strs[0];
+      this.remark = strs[1];
+      this.status = strs[2];
+      this.description = strs[3];
       let catDoc = this.afs.firestore.collection(`product`);
-      catDoc.get().then((querySnapshot) => { 
-          querySnapshot.forEach((doc) => {
-              this.createCat.addToCatList(doc.id,doc.get('name'),doc.get('desc'),doc.get('imagePath'));
-          })
-      })    
-    }else{
-      this.catId=this.item.categoryId;
+      catDoc.get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          this.createCat.addToCatList(doc.id, doc.get('name'), doc.get('desc'), doc.get('imagePath'));
+        })
+      })
+    } else {
+      this.catId = this.item.categoryId;
       let cat = this.categories.find(cat => cat.id === this.catId);
-      this.cat = cat.name;  
-      this.remark=this.item.location;
-      this.status=this.item.status;
-      this.description=this.item.description;
-      str =  this.cat+","+this.remark+","+this.status+","+this.description;
+      this.cat = cat.name;
+      this.remark = this.item.location;
+      this.status = this.item.status;
+      this.description = this.item.description;
+      str = this.cat + "," + this.remark + "," + this.status + "," + this.description;
       localStorage.setItem('currentItem', str);
     }
   }
-  elementType : 'url' | 'canvas' | 'img' = 'url';
+  elementType: 'url' | 'canvas' | 'img' = 'url';
   /*
   value : string = "{\"id\":\""+this.getId()+"\",\"productID\":\""+
                   this.catId+"\",\"remark\":\""+ this.remark+
                   "\",\"status\":\""+this.status+"\"}";
   */
- value : string ="{\"id\":\""+this.itemId+"\"}";
+  value: string = "{\"id\":\"" + this.itemId + "\"}";
 }
 
